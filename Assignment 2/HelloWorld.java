644fe6aa518c89fcdc9906f7c51b271d414a7a45
Samhitha <72470670+samhitha2002@users.noreply.github.com>
2022-08-19 11:15:01 -0400

    Add files via upload

---
+++ Assignment 2/HelloWorld.java
@@ -0,0 +1,21 @@
+/*************************************************************************
+ *  Compilation:  javac HelloWorld.java
+ *  Execution:    java HelloWorld
+ *
+ *  @author: Samhitha Padmanabhini, sp1949@scarletmail.rutgers.edu, sp1949
+ *
+ *  Prints "Hello, World". By tradition, this is everyone's first program.
+ *
+ *  % java HelloWorld
+ *  Hello, World
+ *
+ *************************************************************************/
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello, World");
+    }
+
+}
